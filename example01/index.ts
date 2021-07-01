fb963dbcf50c8779c4a0d17b39139761283bd5e0
Dalton Menezes <daltonmenezes@outlook.com>
2021-07-01 19:24:18 -0300

    Add examples

---
+++ example01/index.ts
@@ -0,0 +1,10 @@
+import path from 'path'
+import { hexToRgb } from 'colors-convert'
+
+console.log(
+  hexToRgb('#ff0000')
+)
+
+console.log(
+  path.resolve()
+)
---
+++ example02/index.js
@@ -0,0 +1,10 @@
+import path from 'path'
+import colors from 'colors-convert'
+
+console.log(
+  hexToRgb('#ff0000')
+)
+
+console.log(
+  path.resolve()
+)
